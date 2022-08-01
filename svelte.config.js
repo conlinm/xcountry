96d71a19566e2ca1f715988fdd0ede5ce073c306
conlinm <conlinm@me.com>
2022-08-01 15:43:45 -0700

    latest

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import appengine from "svelte-adapter-appengine";
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+	//	adapter: adapter(),
+		adapter: appengine(),
+		//target: "#svelte",
 		methodOverride: {
 			allowed: ['PUT', 'DELETE']
 		}
@@ -11,3 +15,5 @@ const config = {
 };
 
 export default config;
+
+// note, I edited this file for goolge app engine accoring to the instructions here: https://github.com/HalfdanJ/svelte-adapter-appengine, after installing the svelte-adapter-appengine package.
